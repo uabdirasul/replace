bf48b4aaff4381673e741964170f6b6101defd96
uabdirasulm <ubbiniyazov04@gmail.com>
2023-04-23 16:57:24 +0500

    create js file write functional part of project

---
+++ script/main.js
@@ -0,0 +1,29 @@
+const textarea = document.querySelector(".text");
+const button = document.querySelector(".copyButton");
+
+const replaceLetter = (text) => {
+  return text.value
+    .replaceAll("U'", "Ú")
+    .replaceAll("A'", "Á")
+    .replaceAll("O'", "Ó")
+    .replaceAll("g'", "ǵ")
+    .replaceAll("o'", "ó")
+    .replaceAll("n'", "ń")
+    .replaceAll("u'", "ú")
+    .replaceAll("a'", "á")
+    .replaceAll("i'", "ı");
+};
+
+const copyContent = async (text) => {
+  if (text.value) {
+    navigator.clipboard.writeText(replaceLetter(textarea));
+    alert(`Kóshirildi: ${text.value.slice(0, 12)}...`);
+  } else {
+    alert("Tekst jazıń");
+  }
+};
+
+button.addEventListener("click", () => {
+  copyContent(textarea);
+  textarea.value = "";
+});
