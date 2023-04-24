4a8cf0332c75e591ca69d27094c886e481f0d4ad
uabdirasulm <ubbiniyazov04@gmail.com>
2023-04-24 06:34:26 +0500

    add function for catching error

--- script/main.js
+++ script/main.js
@@ -2,31 +2,47 @@ const textarea = document.querySelector(".text");
 const button = document.querySelector(".copyButton");
 
 const replaceLetter = (text) => {
-  return text.value
+  return text
     .replaceAll("U'", "Ú")
+    .replaceAll("U’", "Ú")
     .replaceAll("A'", "Á")
+    .replaceAll("A’", "Á")
     .replaceAll("O'", "Ó")
+    .replaceAll("O’", "Ó")
     .replaceAll("N'", "Ń")
+    .replaceAll("N’", "Ń")
     .replaceAll("I'", "Í")
+    .replaceAll("I’", "Í")
     .replaceAll("g'", "ǵ")
+    .replaceAll("g’", "ǵ")
     .replaceAll("o'", "ó")
+    .replaceAll("o’", "ó")
     .replaceAll("n'", "ń")
     .replaceAll("n’", "ń")
     .replaceAll("u'", "ú")
+    .replaceAll("u’", "ú")
     .replaceAll("a'", "á")
-    .replaceAll("i'", "ı");
+    .replaceAll("a’", "á")
+    .replaceAll("i'", "ı")
+    .replaceAll("i’", "ı");
 };
 
 const copyContent = async (text) => {
-  if (text.value) {
-    navigator.clipboard.writeText(replaceLetter(textarea));
-    alert(`Kóshirildi: ${text.value.slice(0, 12)}...`);
+  if (text) {
+    navigator.clipboard
+      .writeText(replaceLetter(textarea.value))
+      .then(() => {
+        alert(`Kóshirildi: ${text.slice(0, 12)}...`);
+      })
+      .catch(() => {
+        alert(`Qátelik boldı`);
+      });
   } else {
     alert("Tekst jazıń");
   }
 };
 
 button.addEventListener("click", () => {
-  copyContent(textarea);
+  copyContent(textarea.value);
   textarea.value = "";
 });
