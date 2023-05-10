c6adbe6e84c920f7273c2d64861e6b81d5c51c15
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-10 19:22:16 +0500

    edit main of site and styles

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -3,14 +3,16 @@ import React, { Component } from "react";
 export default class Main extends Component {
   render() {
     return (
-      <main class="main">
-        <textarea
-          autofocus
-          className="textarea"
-          placeholder="Háriplerdi ózgertiw ushın ' belgisinen paydalanıp jazıń: tami'r => tamır"
-        ></textarea>
-        <button className="copyButton">Kóshirip alıw</button>
-      </main>
+      <div className="container">
+        <main className="main">
+          <textarea
+            className="textarea"
+            autoFocus
+            placeholder="Háriplerdi ózgertiw ushın ' belgisinen paydalanıp jazıń: tami'r => tamır"
+          ></textarea>
+          <button className="copyButton">Kóshirip alıw</button>
+        </main>
+      </div>
     );
   }
 }
