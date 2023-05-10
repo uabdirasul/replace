872071c9fc7354a487031ac4482bdbe58418c1e5
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-10 18:52:55 +0500

    remove old files and init basic react files for v2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hi there</h1>
+      </div>
+    );
+  }
+}
