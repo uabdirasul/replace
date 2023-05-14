053dbe631d4e95144c6240e096cc2cb4637292ef
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-14 16:18:42 +0500

    add function that hides alert

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -75,6 +75,11 @@ export default class Main extends Component {
     });
   };
 
+  hideAlert = () => {
+    this.alertFailRef.current.style.display = "none";
+    this.alertSuccessRef.current.style.display = "none";
+  };
+
   render() {
     const { updateText } = this.state;
     return (
@@ -96,6 +101,7 @@ export default class Main extends Component {
             height="24px"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 384 512"
+            onClick={this.hideAlert}
           >
             <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3l105.4 105.3c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256l105.3-105.4z" />
           </svg>
@@ -119,6 +125,7 @@ export default class Main extends Component {
             height="24px"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 384 512"
+            onClick={this.hideAlert}
           >
             <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3l105.4 105.3c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256l105.3-105.4z" />
           </svg>
